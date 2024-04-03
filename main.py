f6d74e75961afcb266987e53fdd23f686283a5c4
NoahELE <cxhmel@outlook.com>
2024-04-03 21:02:26 +1100

    perf: try new algorithm

--- main.py
+++ main.py
@@ -1,39 +1,12 @@
 from mpi4py import MPI
 
-from utils import DATA_100GB_LINES, get_hour, get_lines, get_sentiment, parse_line
+from utils import merge_and_print_results, process_lines, send_lines
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-size = comm.Get_size()
 
-chunk_size = (DATA_100GB_LINES - 2) // size
-start = 1 + rank * chunk_size
-end = start + chunk_size if rank != size - 1 else DATA_100GB_LINES - 1
-
-lines = get_lines("../twitter-100gb.json", start, end)
-rows = (parse_line(line) for line in lines)
-
-hour_sentiment: dict[int, float] = {}
-day_sentiment: dict[int, float] = {}
-for row in rows:
-    sentiment = get_sentiment(row)
-    if sentiment is None:
-        continue
-    hour = get_hour(row)
-    if hour is None:
-        continue
-    if hour not in hour_sentiment:
-        hour_sentiment[hour] = 0
-    hour_sentiment[hour] += sentiment
-
-# gather data on rank 0
-gathered: list[dict[int, float]] | None = comm.gather(hour_sentiment, root=0)
 if rank == 0:
-    merged: dict[int, float] = {}
-    assert gathered is not None
-    for g in gathered:
-        for k, v in g.items():
-            if k not in merged:
-                merged[k] = 0
-            merged[k] += v
-    print("happiest hour: ", max(merged.keys(), key=lambda k: (merged[k])))
+    send_lines("../twitter-100gb.json", comm)
+    merge_and_print_results(comm)
+else:
+    process_lines(comm)
--- utils.py
+++ utils.py
@@ -1,25 +1,107 @@
-import itertools
+from collections import defaultdict
 from datetime import datetime
 from typing import Any
 
 import orjson
+from mpi4py.MPI import Intracomm
 
-DATA_100GB_LINES = 100000001
 
-
-def get_lines(file: str, start: int, end: int) -> list[str]:
-    """read the lines of data file (skip the first and last line)"""
+def send_lines(file: str, comm: Intracomm) -> None:
+    """
+    send the lines of data file from rank 0 to other ranks
+    (skip the first and last line)
+    """
     with open(file, encoding="utf-8") as f:
-        # get the lines
-        lines = itertools.islice(f, start, end)
+        # get the lines (skip the first and last line)
+        lines = f.readlines()[1:-1]
+
+    size = comm.Get_size()
+    ranks = range(1, size)
+    # send the lines to ranks 1 to size - 1
+    i = 0
+    for line in lines:
         # remove trailing comma and newline
-        lines = [line.rstrip(",\n") for line in lines]
-        return lines
+        line = line.rstrip(",\n")
+        comm.send(line, dest=ranks[i])
+        # move to the next rank (skip rank 0)
+        i = (i + 1) % len(ranks)
+    # send None to all ranks to indicate the end of the data
+    for r in ranks:
+        comm.send(None, dest=r)
 
 
-def parse_line(line: str) -> dict[str, Any]:
-    """parse the line into a dict"""
-    return orjson.loads(line)
+def process_lines(
+    comm: Intracomm,
+) -> None:
+    """process the lines of data sent by rank 0"""
+    hour_sentiment: dict[datetime, float] = defaultdict(float)
+    day_sentiment: dict[datetime, float] = defaultdict(float)
+    hour_tweets: dict[datetime, int] = defaultdict(int)
+    day_tweets: dict[datetime, int] = defaultdict(int)
+    # process the lines until a empty string is received
+    while (line := comm.recv(source=0)) is not None:
+        row = orjson.loads(line)
+        sentiment = get_sentiment(row)
+        hour = get_hour(row)
+        day = get_day(row)
+        if hour is not None:
+            hour_tweets[hour] += 1
+            if sentiment is not None:
+                hour_sentiment[hour] += sentiment
+        if day is not None:
+            day_tweets[day] += 1
+            if sentiment is not None:
+                day_sentiment[day] += sentiment
+    # send the results to rank 0
+    comm.send(
+        (
+            hour_sentiment,
+            day_sentiment,
+            hour_tweets,
+            day_tweets,
+        ),
+        dest=0,
+    )
+
+
+def merge_and_print_results(comm: Intracomm) -> None:
+    """merge the results from all ranks"""
+    size = comm.Get_size()
+    results = []
+    for i in range(1, size):
+        results.append(comm.recv(source=i))
+    # merge the results
+    merged_hour_sentiment: dict[datetime, float] = defaultdict(float)
+    merged_day_sentiment: dict[datetime, float] = defaultdict(float)
+    merged_hour_tweets: dict[datetime, int] = defaultdict(int)
+    merged_day_tweets: dict[datetime, int] = defaultdict(int)
+    for (
+        hour_sentiment,
+        day_sentiment,
+        hour_tweets,
+        day_tweets,
+    ) in results:
+        for hour, sentiment in hour_sentiment.items():
+            merged_hour_sentiment[hour] += sentiment
+        for day, sentiment in day_sentiment.items():
+            merged_day_sentiment[day] += sentiment
+        for hour, tweets in hour_tweets.items():
+            merged_hour_tweets[hour] += tweets
+        for day, tweets in day_tweets.items():
+            merged_day_tweets[day] += tweets
+    # print the results
+    print(
+        f"happiest hour: {max(merged_hour_sentiment, key=lambda k :merged_hour_sentiment[k])}"
+    )
+    print(
+        f"happiest day: {max(merged_day_sentiment, key=lambda k :merged_day_sentiment[k])}"
+    )
+    print(
+        f"most active hour: {max(merged_hour_tweets, key=lambda k :merged_hour_tweets[k])}"
+    )
+    print(
+        f"most active day: {max(merged_day_tweets, key=lambda k :merged_day_tweets[k])}"
+    )
 
 
 def get_sentiment(row: dict[str, Any]) -> float | None:
@@ -36,21 +118,21 @@ def get_sentiment(row: dict[str, Any]) -> float | None:
         return None
 
 
-def get_hour(row: dict[str, Any]) -> int | None:
+def get_hour(row: dict[str, Any]) -> datetime | None:
     """get the hour from the row"""
     try:
         datetime_str = row["doc"]["data"]["created_at"]
         parsed = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%fZ")
-        return parsed.hour
+        return parsed.replace(minute=0, second=0, microsecond=0)
     except KeyError:
         return None
 
 
-def get_day(row: dict[str, Any]) -> int | None:
+def get_day(row: dict[str, Any]) -> datetime | None:
     """get the day from the row"""
     try:
         datetime_str = row["doc"]["data"]["created_at"]
         parsed = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%fZ")
-        return parsed.day
+        return parsed.replace(hour=0, minute=0, second=0, microsecond=0)
     except KeyError:
         return None
