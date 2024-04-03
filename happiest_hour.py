1c6d91a763c6735074f51cdf8742ea407c8a4743
NoahELE <cxhmel@outlook.com>
2024-04-03 13:20:54 +1100

    feat: try 50mb data

--- happiest_hour.py
+++ happiest_hour.py
@@ -8,7 +8,7 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-lines = get_lines("../twitter-1mb.json")
+lines = get_lines("../twitter-50mb.json")
 
 chunk_size = len(lines) // size
 start = rank * chunk_size
@@ -34,10 +34,9 @@ gathered: list[dict[int, list[float]]] | None = comm.gather(hour_sentiment, root
 if rank == 0:
     merged: dict[int, list[float]] = {}
     assert gathered is not None
-    print("gathered: ", gathered[0])
     for g in gathered:
         for k, v in g.items():
             if k not in merged:
                 merged[k] = []
             merged[k].extend(v)
-    print("result: ", max(merged.keys(), key=lambda k: statistics.mean(merged[k])))
+    print("happiest hour: ", max(merged.keys(), key=lambda k: statistics.mean(merged[k])))
