6bbfa7049edfb0be3a56064f074b669953a61d40
NoahELE <cxhmel@outlook.com>
2024-04-10 20:46:26 +1000

    chore: update plotting data

--- plot.py
+++ plot.py
@@ -4,12 +4,12 @@ import seaborn as sns
 
 sns.set_theme(style="whitegrid")
 
-time_2n8c = "9m40.786s"
-time_1n8c = "10m7.947s"
-time_1n1c = "39m14.164s"
+time_1n1c = "39m32.343s"
+time_1n8c = "9m55.108s"
+time_2n8c = "10m47.546s"
 
 
-def calculate_time_as_seconds(time_str: str) -> float:
+def parse_time_as_seconds(time_str: str) -> float:
     minutes = float(time_str[: time_str.index("m")])
     seconds = float(time_str[time_str.index("m") + 1 : time_str.index("s")])
     return minutes * 60 + seconds
@@ -19,9 +19,9 @@ df = pl.DataFrame(
     {
         "config": ["1 node 1 core", "1 node 8 cores", "2 nodes 8 cores"],
         "time": [
-            calculate_time_as_seconds(time_1n1c),
-            calculate_time_as_seconds(time_1n8c),
-            calculate_time_as_seconds(time_2n8c),
+            parse_time_as_seconds(time_1n1c),
+            parse_time_as_seconds(time_1n8c),
+            parse_time_as_seconds(time_2n8c),
         ],
     }
 )
