3d19588a74044eca3b8e4e3ae6fcd74162338543
NoahELE <cxhmel@outlook.com>
2024-04-11 14:39:30 +1000

    feat: add time measurement

--- process_twitter_data1.py
+++ process_twitter_data1.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from collections import defaultdict
 from datetime import datetime
 from typing import Any, Generator
@@ -33,13 +34,16 @@ size = comm.size
 
 filename = sys.argv[1]
 
+start = time.time()
 if rank == 0:
     total_lines = count_lines(filename)
 else:
     total_lines = 0
 total_lines = comm.bcast(total_lines, root=0)
+print(f"Time for counting lines: {time.time() - start}")
 
 
+start = time.time()
 lines = read_lines(filename, total_lines, rank, size)
 hour_sentiment: dict[datetime, float] = defaultdict(float)
 day_sentiment: dict[datetime, float] = defaultdict(float)
@@ -62,7 +66,9 @@ for line in lines:
         hour_tweets[hour] += 1
         if sentiment is not None:
             hour_sentiment[hour] += sentiment
+print(f"Time for processing lines: {time.time() - start}")
 
+start = time.time()
 hour_sentiment_list = comm.gather(hour_sentiment, root=0)
 day_sentiment_list = comm.gather(day_sentiment, root=0)
 hour_tweets_list = comm.gather(hour_tweets, root=0)
@@ -101,3 +107,4 @@ if rank == 0:
     print(
         f"most active day: {max(merged_day_tweets, key=lambda k :merged_day_tweets[k])}"
     )
+    print(f"Time for merging results: {time.time() - start}")
